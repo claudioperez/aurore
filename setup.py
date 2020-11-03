093d4d808243fe25a2e84a8c8b787cb1525d922e
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2020-11-02 23:10:46 -0800

    cmp

---
+++ setup.py
@@ -0,0 +1,43 @@
+import io
+from os.path import splitext, basename, join, dirname
+from glob import glob
+
+from setuptools import Extension
+from setuptools import find_packages
+from setuptools import setup
+
+
+def read(*names, **kwargs):
+    with io.open(
+        join(dirname(__file__), *names),
+        encoding=kwargs.get('encoding', 'utf8')
+    ) as fh:
+        return fh.read()
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="aurore",
+    version="0.0.1",
+    author="Claudio Perez",
+    author_email="claudio_perez@berkeley.edu",
+    description="Utilities",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    #url="https://github.com/pypa/sampleproject",
+    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    include_package_data=True,
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    # install_requires = ['ruamel.yaml'],
+    entry_points={
+        'console_scripts': [
+            'aurore = aurore.__main__:main',
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
